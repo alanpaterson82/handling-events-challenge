72cc4bf8de7637be99abce6b2c1dcc793c5c4948
alanpaterson82 <alan_paterson82@hotmail.com>
2024-03-11 10:50:14 +0000

    challenge 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
-import css from './App.module.css';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <NavBarForm />
       <Sidebar />
     </div>
   );
